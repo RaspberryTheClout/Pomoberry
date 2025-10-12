0c29e67fb0abaca4db1090fa9d6babc5ba36f39d
RaspberryClout <raspberryclout@gmail.com>
2025-10-12 13:42:34 +0200

    feat: Implement functional UI and complete v1.0

--- Pomoberry.UI/MainWindow.xaml.cs
+++ Pomoberry.UI/MainWindow.xaml.cs
@@ -1,24 +1,59 @@
-﻿using System.Text;
+﻿using Pomoberry.Core;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace PomodoroTimer.UI
+namespace Pomoberry.UI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly PomodoroSessionManager _manager;
+
         public MainWindow()
         {
             InitializeComponent();
+            _manager = new PomodoroSessionManager();
+
+            // Subscribe to events from the core logic
+            _manager.TimeChanged += Manager_TimeChanged;
+            _manager.SessionStarted += Manager_SessionStarted;
+        }
+
+        private void Manager_SessionStarted(object? sender, SessionEventArgs e)
+        {
+            // The timer's events run on a background thread.
+            // To update the UI, we must use the Dispatcher to send the work to the UI thread.
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                SessionDisplay.Text = e.SessionType.ToString();
+            });
+        }
+
+        private void Manager_TimeChanged(object? sender, TimeChangedEventArgs e)
+        {
+            // Use the Dispatcher here as well for thread safety.
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                TimeDisplay.Text = e.TimeLeft.ToString(@"mm\:ss");
+            });
+        }
+
+        // --- Button Click Handlers ---
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            _manager.Start();
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            _manager.Pause();
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            _manager.Reset();
         }
     }
 }
