f6b79cf0cbb0cf74b249e6ab07f87ee9a34d1460
RaspberryClout <raspberryclout@gmail.com>
2025-10-13 18:19:20 +0200

    WIP: Add initial AudioManager for background music

---
+++ Pomoberry.Core/AudioManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Media;
+using System.IO;
+using System.Runtime.Versioning;
+
+namespace Pomoberry.Core
+{
+    [SupportedOSPlatform("windows")]
+    public class AudioManager : IDisposable
+    {
+        private readonly SoundPlayer _player;
+
+        public AudioManager(string soundFileName)
+        {
+            // Expects the sound file to be in the same directory as the executable.
+            string soundPath = Path.Combine(AppContext.BaseDirectory, soundFileName);
+
+            if (File.Exists(soundPath))
+            {
+                _player = new SoundPlayer(soundPath);
+                _player.Load(); // Pre-load the sound file for faster playback
+            }
+            else
+            {
+                // If the file is not found, create a player that does nothing.
+                // Prevents the app from crashing if the sound file is missing.
+                _player = new SoundPlayer();
+            }
+        }
+
+        public void PlayMusic()
+        {
+            // Loop the sound so it plays continuously during the work session.
+            _player?.PlayLooping();
+        }
+
+        public void StopMusic()
+        {
+            _player?.Stop();
+        }
+
+        public void Dispose()
+        {
+            _player?.Dispose();
+        }
+    }
+}
